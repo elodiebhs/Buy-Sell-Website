3e447da7bd2bcab1a8d3867a921837d7e3bb77ec
elodiebhs <elodiebouthors@hotmail.com>
2021-03-07 22:54:00 +0000

    updating queries

--- 1_queries/4_adminPage.sql
+++ 1_queries/4_adminPage.sql
@@ -1,1 +1,8 @@
-??
+---see all products
+SELECT products.id, products.title, thumbnail_photo
+FROM products
+WHERE sold = false
+
+
+--- delete a product
+--- edit a product
--- 1_queries/8_searchPage.sql
+++ 1_queries/8_searchPage.sql
@@ -1,3 +1,9 @@
 --Get all products search page 
 
-SELECT (products.*)
+SELECT products.*
+FROM products
+WHERE ---
+AND brand LIKE ---
+AND size = ---
+AND price >= ---
+AND price <= ---
