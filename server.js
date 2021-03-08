e5098986e783d0d8fdde89f5608b5d2aa648ce9f
elodiebhs <elodiebouthors@hotmail.com>
2021-03-08 02:57:49 +0000

    small changes

--- server.js
+++ server.js
@@ -51,16 +51,6 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/login", (req, res) => {
-  
-  res.render("login");
-});
-
-app.get("/admin", (req, res) => {
-  
-  res.render("admin");
-});
-
 
 
 app.listen(PORT, () => {
