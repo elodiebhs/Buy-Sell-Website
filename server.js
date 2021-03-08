4e3aa092f43c180ff4307df8946fb384d2eb44d9
elodiebhs <elodiebouthors@hotmail.com>
2021-03-08 00:39:02 +0000

    update header and login

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
+  
   res.render("index");
 });
 
