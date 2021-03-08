7cdbe2cf9293a77b388b2f49d681c2713b9a79e3
elodiebhs <elodiebouthors@hotmail.com>
2021-03-08 23:32:51 +0000

    add components to ejs

--- server.js
+++ server.js
@@ -41,7 +41,6 @@ const productRoutes = require("./routes/product-router");
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/products", productRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
 
 // Note: mount other resources here, using the same pattern above
 
