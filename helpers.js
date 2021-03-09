523c7fa80e629ef2cdc0f85b118d59dfb17edefe
Kevin Phan <ken.phan@live.ca>
2021-03-09 00:54:47 +0000

    logout clear cookies

--- helpers.js
+++ helpers.js
@@ -1,3 +1,17 @@
+// USER DATABASE (TEMP)
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
 // HELPER FUNCTIONS
 // Returns true if email exists
 function checkEmailExists(email){
