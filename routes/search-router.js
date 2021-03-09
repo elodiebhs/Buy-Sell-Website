759d63638c149b02f15f62ac482addcd0465a2df
Neil Oribello <neil.oribello@gmail.com>
2021-03-09 00:55:02 +0000

    Monday 5pmm hourly merge

--- routes/search-router.js
+++ routes/search-router.js
@@ -4,6 +4,14 @@ const router  = express.Router();
 module.exports = (db) => {
   router.get("/search", (req, res) => {
     console.log("Search:", req.query);
+    // db.query(`
+    // SELECT * FROM products WHERE brand = $1 AND price >= $2 AND price <= $3;
+    // `, ["%"+req.query.productName+"%", ((req.query.minPrice)*100), ((req.query.maxPrice)*100)])
+    // .then(data => {
+    //   const templateVars = { products: data.rows, productName: req.query.productName }
+    //   console.log("search results", templateVars.products)
+    // })
+    
     res.render("search")
   });
   return router;
