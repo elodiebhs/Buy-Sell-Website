c121029527a4134366d91d7627f5e880e643e500
Neil Oribello <neil.oribello@gmail.com>
2021-03-09 20:29:35 +0000

    added search-router kinda working

--- routes/search-router.js
+++ routes/search-router.js
@@ -3,9 +3,13 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/search", (req, res) => {
-    console.log("Search objects", req.query)
-    db.query(`SELECT * FROM products WHERE brand ILIKE $1 AND size LIKE $2 AND price >= $3 AND price <= $4;`,["%"+req.query.productBrand+"%", ((req.query.minPrice)*100), ((req.query.maxPrice)*100)] )
-    res.render("search")
+    console.log("Search object", req.query);
+    db.query(`SELECT * FROM products WHERE price >= ${req.query.minPrice} AND price <= ${req.query.maxPrice};`)
+    .then(data => {
+      console.log("data rows: ", data.rows);
+      const templateVars = { products: data.rows }
+      res.render("search", templateVars)
+    })
   });
   return router;
 }
