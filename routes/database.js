ed16f97cf4285d852d8bc65f5cb9a83c243e1a9e
Neil Oribello <neil.oribello@gmail.com>
2021-03-10 00:23:11 +0000

    SEARCH IS WORKING!

--- routes/database.js
+++ routes/database.js
@@ -10,19 +10,42 @@ const getUserWithEmail = function(email) {
   .catch(err => console.log(err.stack));
 }
 
-// const getAllProducts = function(options) {
-//   const queryParams = [];
+const getAllProducts = function(db, options) {
+  const queryParams = [];
 
-//   let queryString = `SELECT * FROM products`;
+  let queryString = `SELECT * FROM products `;
 
-//   if (options.minPrice) {
-//     if(queryParams.length > 0) {
+  if (options.minPrice) {
+    if(queryParams.length > 0) {
+      queryString += `AND `;
+    } else {
+      queryString += `WHERE `;
+    }
 
-//     }
-//   }
-// }
+    queryParams.push(`${options.minPrice}`);
+    queryString += `price >= $${queryParams.length} `;
+  }
+
+  if (options.maxPrice) {
+    if(queryParams.length > 0) {
+      queryString += `AND `;
+    } else {
+      queryString += `WHERE `;
+    }
+
+    queryParams.push(`${options.maxPrice}`);
+    queryString += `price <= $${queryParams.length}`;
+  }
+  queryString += `;`;
+  console.log("querystring:", queryString, "query params:", queryParams);
+  
+  return db
+    .query(queryString, queryParams)
+    .then(res => res.rows)
+    .catch(err => err);
+}
 
 
-module.exports = getUserWithEmail;
+module.exports = getAllProducts;
 
 
