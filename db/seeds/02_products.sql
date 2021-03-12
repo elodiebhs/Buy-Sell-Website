12235183891f3a60c6bce0168265ee0e91fdb766
Neil Oribello <neil.oribello@gmail.com>
2021-03-12 02:19:11 +0000

    css changes

--- db/seeds/02_products.sql
+++ db/seeds/02_products.sql
@@ -3,15 +3,15 @@
 INSERT INTO products( user_id, title, description, thumbnail_photo, main_photo, brand,
 size, price, feature)
 VALUES
-( 1, 'Air Jordan', 'Air Jordan grey and orange', 'https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Nike',9, 550, false ),
+( 1, 'Jordan 1', 'University Blue', 'https://image.goat.com/1000/attachments/product_template_pictures/images/049/887/651/original/599627_00.png.png', 'https://image.goat.com/1000/attachments/product_template_pictures/images/049/887/651/original/599627_00.png.png', 'Nike',9, 550, false ),
 
-( 1, 'White Air Jordan', 'Air Jordan white and red', 'https://images.pexels.com/photos/2385477/pexels-photo-2385477.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'https://images.pexels.com/photos/2385477/pexels-photo-2385477.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Nike', 8, 700, false ),
+( 1, 'Jordan 4', 'Purple Metallic', 'https://image.goat.com/crop/375/attachments/product_template_additional_pictures/images/037/189/335/original/553685_01.jpg.jpeg?1590599390', 'https://image.goat.com/crop/375/attachments/product_template_additional_pictures/images/037/189/335/original/553685_01.jpg.jpeg?1590599390', 'Jordan', 8, 700, false ),
 
-( 1, 'Adidas Yeezy Boost', 'Gray Adidas Yeezy Boost 350', 'https://images.pexels.com/photos/1280064/pexels-photo-1280064.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'https://images.pexels.com/photos/1280064/pexels-photo-1280064.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Adidas', 9, 300, false ),
+( 1, 'Reebok Club C', 'Tom & Jerry All-Over', 'https://images.stockx.com/images/Reebok-Club-C-Tom-Jerry-Tom.png?fit=clip&bg=FFFFFF&h=500&auto=compress&q=90&dpr=2&trim=color&updated_at=1613063868&fm=webp&ixlib=react-9.0.3&w=1074', 'https://images.stockx.com/images/Reebok-Club-C-Tom-Jerry-Tom.png?fit=clip&bg=FFFFFF&h=500&auto=compress&q=90&dpr=2&trim=color&updated_at=1613063868&fm=webp&ixlib=react-9.0.3&w=1074', 'Reebok', 9, 300, false ),
 
-( 1, 'Puma', 'White Puma Clyde', 'https://images.pexels.com/photos/2759783/pexels-photo-2759783.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'https://images.pexels.com/photos/2759783/pexels-photo-2759783.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Puma', 7, 450, false ),
+( 1, 'Puma Wmns RS-Fast', 'Sunset - Peachskin', 'https://image.goat.com/crop/375/attachments/product_template_pictures/images/050/366/747/original/375787_02.png.png', 'https://image.goat.com/crop/375/attachments/product_template_pictures/images/050/366/747/original/375787_02.png.png', 'Puma', 7, 450, true ),
 
-( 1, 'Vans', 'Vans orange', 'https://images.pexels.com/photos/1598508/pexels-photo-1598508.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940','https://images.pexels.com/photos/1598508/pexels-photo-1598508.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Vans', 8, 90, true ),
+( 1, 'New Balance', 'White team red', 'https://image.goat.com/crop/375/attachments/product_template_additional_pictures/images/048/707/974/original/708099_01.jpg.jpeg?1610475864','https://image.goat.com/crop/375/attachments/product_template_additional_pictures/images/048/707/974/original/708099_01.jpg.jpeg?1610475864', 'New Balance', 8, 90, true ),
 
-( 1, 'White Adidas', 'White running shoes', 'https://images.pexels.com/photos/1670766/pexels-photo-1670766.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940','https://images.pexels.com/photos/1598508/pexels-photo-1598508.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Adidas', 8, 150, true );
+( 1, 'Yeezy Boost 350 V2', 'Triple White', 'https://image.goat.com/crop/375/attachments/product_template_additional_pictures/images/014/822/692/original/116662_01.jpg.jpeg?1537556579','https://image.goat.com/crop/375/attachments/product_template_additional_pictures/images/014/822/692/original/116662_01.jpg.jpeg?1537556579', 'Adidas', 8, 150, true );
 
