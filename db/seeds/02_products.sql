fc2da12df48245c7eae315c789aa11cea22573c3
Neil Oribello <neil.oribello@gmail.com>
2021-03-12 04:51:33 +0000

    css/template changes

--- db/seeds/02_products.sql
+++ db/seeds/02_products.sql
@@ -1,15 +1,15 @@
 INSERT INTO products( user_id, title, description, thumbnail_photo, main_photo, brand,
 size, price, feature)
 VALUES
-( 1, 'Air Jordan 1', 'University Blue', 'https://image.goat.com/1000/attachments/product_template_pictures/images/049/887/651/original/599627_00.png.png', 'https://image.goat.com/1000/attachments/product_template_pictures/images/049/887/651/original/599627_00.png.png', 'Nike', 9, 550, false ),
+( 1, 'Air Jordan 1', 'University Blue', 'https://image.goat.com/1000/attachments/product_template_pictures/images/008/654/832/original/31395_00.png.png', 'https://image.goat.com/1000/attachments/product_template_pictures/images/008/654/832/original/31395_00.png.png', 'Nike', 9, 550, false ),
 
-( 1, 'Nike Dunk Low Retro White Black (2021)', 'White Black', 'https://stockx-360.imgix.net/Nike-Dunk-Low-Retro-White-Black-2021/Images/Nike-Dunk-Low-Retro-White-Black-2021/Lv2/img01.jpg', 'https://stockx-360.imgix.net/Nike-Dunk-Low-Retro-White-Black-2021/Images/Nike-Dunk-Low-Retro-White-Black-2021/Lv2/img01.jpg', 'Nike', 8, 350, false ),
+( 1, 'Nike Dunk Low Retro White Black (2021)', 'White Black', 'https://image.goat.com/750/attachments/product_template_pictures/images/051/507/240/original/719082_00.png.png', 'https://image.goat.com/750/attachments/product_template_pictures/images/051/507/240/original/719082_00.png.png', 'Nike', 8, 350, false ),
 
-( 1, 'Nike Dunk Low Pink Pearl', 'Pink Pearl White', 'https://stockx-360.imgix.net/Nike-Dunk-Low-Orange-Pearl/Images/Nike-Dunk-Low-Orange-Pearl/Lv2/img01.jpg', 'https://stockx-360.imgix.net/Nike-Dunk-Low-Orange-Pearl/Images/Nike-Dunk-Low-Orange-Pearl/Lv2/img01.jpg', 'Nike', 9, 100, false ),
+( 1, 'Nike Dunk Low Pink Pearl', 'Pink Pearl White', 'https://image.goat.com/1000/attachments/product_template_pictures/images/050/396/292/original/721459_00.png.png', 'https://image.goat.com/1000/attachments/product_template_pictures/images/050/396/292/original/721459_00.png.png', 'Nike', 9, 100, false ),
 
-( 1, 'Puma RS-X3 Puzzle Puma White', 'White Red', 'https://stockx-360.imgix.net/Puma-RS-X3-Puzzle-Puma-White/Images/Puma-RS-X3-Puzzle-Puma-White/Lv2/img01.jpg', 'https://stockx-360.imgix.net/Puma-RS-X3-Puzzle-Puma-White/Images/Puma-RS-X3-Puzzle-Puma-White/Lv2/img01.jpg', 'Puma', 7, 450, true ),
+( 1, 'Puma RS-X3 Puzzle Puma White', 'White Red', 'https://image.goat.com/1000/attachments/product_template_pictures/images/044/519/093/original/562032_00.png.png', 'https://image.goat.com/1000/attachments/product_template_pictures/images/044/519/093/original/562032_00.png.png', 'Puma', 7, 450, true ),
 
-( 1, 'New Balance 550 White Red', 'White Red', 'https://images.stockx.com/images/New-Balance-550-White-Red.png?f','https://images.stockx.com/images/New-Balance-550-White-Red.png?f', 'New Balance', 8, 240, true ),
+( 1, 'New Balance 550 White Red', 'White Red', 'https://image.goat.com/1000/attachments/product_template_pictures/images/048/707/967/original/708099_00.png.png','https://image.goat.com/1000/attachments/product_template_pictures/images/048/707/967/original/708099_00.png.png', 'New Balance', 8, 240, true ),
 
-( 1, 'Adidas Yeezy Boost 350 V2', 'Triple White', 'https://stockx-360.imgix.net/adidas-Yeezy-Boost-350-V2-Cream-White/Images/adidas-Yeezy-Boost-350-V2-Cream-White/Lv2/img01.jpg','https://stockx-360.imgix.net/adidas-Yeezy-Boost-350-V2-Cream-White/Images/adidas-Yeezy-Boost-350-V2-Cream-White/Lv2/img01.jpg', 'Adidas', 8, 440, true );
+( 1, 'Adidas Yeezy Boost 350 V2', 'Triple White', 'https://image.goat.com/1000/attachments/product_template_pictures/images/014/822/680/original/116662_00.png.png','https://image.goat.com/1000/attachments/product_template_pictures/images/014/822/680/original/116662_00.png.png', 'Adidas', 8, 440, true );
 
